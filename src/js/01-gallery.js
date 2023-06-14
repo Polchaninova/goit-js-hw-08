543ce88e0abcfe9ad00f50cd97ee3cec89f13af0
Olha Polchaninova <polchaninovaolha089@gmail.com>
2023-06-14 16:23:53 +0300

    feat: completed task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,32 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import "simplelightbox/dist/simple-lightbox.min.css";
 // Change code below this line
 
 console.log(galleryItems);
+
+const galleryItemsContainer = document.querySelector(".gallery");
+const cardsMarkup = createGalleryItemsMarkup(galleryItems);
+// console.log(cardsMarkup);
+
+galleryItemsContainer.insertAdjacentHTML("beforeend", cardsMarkup);
+
+function createGalleryItemsMarkup(galleryItems) {
+  return galleryItems.map(({ preview, original, description }) => {
+    return `
+
+    <li class="gallery__item">
+       <a class="gallery__link" href="${original}">
+       <img loading="lazy"
+       class="gallery__image"
+       src="${preview}"
+       alt="${description}"
+       />
+   </a>
+</li>`
+  }).join("")
+}
+// Usage
+const gallery__item = new SimpleLightbox('.gallery__item a', { captionsData: "alt", captionDelay: 250, captionPosition: 'bottom' });
+
