cdf7f6933deabefe5a14e6871db8e4e2ae063ea7
Olha Polchaninova <polchaninovaolha089@gmail.com>
2023-06-17 13:44:07 +0300

    completed task 3

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,30 @@
+{/* <iframe
+  id="vimeo-player"
+  src="https://player.vimeo.com/video/236203659"
+  width="640"
+  height="360"
+  frameborder="0"
+  allowfullscreen
+  allow="autoplay; encrypted-media"
+></iframe> */}
+// В HTML есть < iframe > с видео для Vimeo плеера.
+// Напиши скрипт который будет сохранять текущее время воспроизведения видео
+// в локальное хранилище и,
+// при перезагрузке страницы, продолжать воспроизводить видео с этого времени.
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Vimeo.Player(iframe);
+
+player.on('play', function () {
+  console.log('played the video!');
+});
+const onPlay = function (data) {
+  // data is an object containing properties specific to that event
+};
+
+player.on('play', onPlay);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,73 @@
 
+{/* <form class="feedback-form" autocomplete="off">
+  <label>
+    Email
+    <input type="email" name="email" autofocus />
+  </label>
+  <label>
+    Message
+    <textarea name="message" rows="8"></textarea>
+  </label>
+  <button type="submit">Submit</button>
+</form> */}
+import throttle from 'lodash.throttle';
+
+const refs = {
+  form: document.querySelector(".feedback-form"),
+  email: document.querySelector(".feedback-form input"),
+  textarea: document.querySelector(".feedback-form textarea")
+}
+const STORAGE_KEY = 'feedback-form-state';
+const formData = {
+  email: "",
+  message: ""
+}
+
+refs.form.addEventListener("submit", onFormSubmit);
+refs.email.addEventListener("input", throttle(onTextareaInput, 500));
+refs.textarea.addEventListener('input', throttle(onTextareaInput, 500));
+
+populateTextarea()
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log("User Submit form");
+  e.currentTarget.reset()// reset form =>
+  console.log(localStorage.getItem(STORAGE_KEY));
+  // !if form submission => removeItem on key, so that - users not submit again
+  localStorage.removeItem(STORAGE_KEY)// reset localStorage
+}
+
+function onTextareaInput(e) {
+  // const message = e.target.value;
+  const name = e.target.name;
+  const email = e.target.value;
+  formData[name] = email;
+  console.log(formData);
+  //            method      key    :     value
+  localStorage.setItem(STORAGE_KEY, JSON.stringify(formData))
+}
+
+function populateTextarea() {
+  // *get value in localSt
+  //                                            key
+  // *save value to localStorage
+  const savedFormData = localStorage.getItem(STORAGE_KEY);
+  let parsedFormData
+  try {
+    parsedFormData = JSON.parse(savedFormData);
+    console.log(parsedFormData); // settings object
+  } catch (error) {
+    console.log(error);
+  }
+  // !check - Is user entered something, but not submit? (true=>pass value to textarea || false=>ignore)
+  //      true
+  if (parsedFormData) {
+    console.log(parsedFormData);
+    // pass value to textarea
+    refs.email.value = parsedFormData.email;
+    refs.textarea.value = parsedFormData.message;
+    Object.assign(formData, parsedFormData)
+  }
+}
